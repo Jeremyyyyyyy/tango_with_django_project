4cb98eefee186f25064e28936fdba09267da07b4
Jeremyyyyyyy <80373243+Jeremyyyyyyy@users.noreply.github.com>
2022-02-24 11:07:36 +0800

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     # chapter 3
@@ -27,10 +28,13 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['extra'] = 'From the model solution on GitHub'
+    # context_dict['extra'] = 'From the model solution on GitHub'
+
+    visitor_cookie_handler(request)
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
-    
+    return response
+
 def about(request):
     # chapter 3
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>") 
@@ -40,7 +44,12 @@ def about(request):
     print(request.method)
      # prints out the user name, if no one is logged in it prints `AnonymousUser`
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
 # Create a context dictionary which we can pass
@@ -213,8 +222,8 @@ def user_login(request):
 # The request is not a HTTP POST, so display the login form.
 # This scenario would most likely be a HTTP GET.
     else:
-    # No context variables to pass to the template system, hence the
-    # blank dictionary object...
+# No context variables to pass to the template system, hence the
+# blank dictionary object...
         return render(request, 'rango/login.html')
 
 def some_view(request):
@@ -234,3 +243,19 @@ def user_logout(request):
 # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
